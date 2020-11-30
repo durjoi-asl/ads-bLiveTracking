c6a432ddc166c6a5b89997473a2c1ce2b0471fcc
Mehedi Hassan Durjoi <durjoi@asl.aero>
2020-11-30 17:47:03 +0600

    Google map live tracking added

--- src/App.js
+++ src/App.js
@@ -29,18 +29,34 @@ class App extends Component {
     }), 1000)
     
   }
-  // componentDidMount() {
-  //   this.interval = setInterval(() => this.setState({ aircraft:{ lat: this.state.aircraft.lat+0.005, lng: this.state.aircraft.lng } }), 1000);
-  // }
+  
   componentWillUnmount() {
     clearInterval(this.interval);
   }
   
   displayAircraft = () => {
+    const google = window.google
     return this.state.aircrafts.data.map((aircraft, index) => {
-      return <Marker key={index} id={index} position={{lat: aircraft.lat, lng: aircraft.lon}}/>
+      return <Marker key={index} id={index} position={{lat: aircraft.lat, lng: aircraft.lon}}
+        icon={{
+          path: "M510,255c0-20.4-17.85-38.25-38.25-38.25H331.5L204,12.75h-51l63.75,204H76.5l-38.25-51H0L25.5,255L0,344.25h38.25l38.25-51h140.25l-63.75,204h51l127.5-204h140.25C492.15,293.25,510,275.4,510,255z",
+          fillColor: '#FF0000',
+          fillOpacity: 1,
+          scaledSize: new google.maps.Size(1, 1),
+          strokeWeight: 0,
+          scale: .04,
+          rotation: aircraft.trk - 90
+        }}
+      />
     })  
   }
+
+  // icon={{
+  //   url: vector,
+  //   anchor: new google.maps.Point(17, 46),
+  //   scaledSize: new google.maps.Size(20, 20),
+  //   rotation: 360,
+  // }}
   
   
   
